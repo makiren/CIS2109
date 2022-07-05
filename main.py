cbd71f64a899b0ddf6d150677d92af768bdb6ade
makiren <makishima@initie.tokyo>
2022-07-05 22:59:37 +0900

    Refactoring

--- main.py
+++ main.py
@@ -6,12 +6,41 @@ import pymysql as pymy
 # Refer to StudentManagement.jpg
 # for the database design that is assumed on this project.
 
-# Functions Start -----
+
+
+# Start of element assignment of GUI
+root = tkinter.Tk()
+
+root.title("TUJ Student Repository")
+root.geometry('1024x768')
+
+frame_left = tkinter.Frame(root, bg='#9D2235', width="724", height="768") # Temple Color
+frame_right = tkinter.Frame(root, bg='gray', width="300", height="768")
+frame_right.pack(side="right")
+frame_left.pack(side="left")
+
+
+global text # global text for label
+text = 'This is text variable'
+label_head = tkinter.Label(frame_left, text=text,
+                           font=('Lucida Console', '25', 'bold'), justify='center', fg='white', bg='maroon',
+                           anchor='center')  # Not placed, just declared!
+
+
+# TU Logo
+tu_image = tkinter.PhotoImage(file='temple-logo-t-box.png')
+disp_image = tkinter.Label(frame_left, image=tu_image)
+disp_image.place(anchor="n", x=362, y=0)
+# Welcome message
+welcome = tkinter.Label(frame_left, text='Welcome to TUJ Student Repository',
+                        justify='center', fg='white', bg='black',
+                        anchor='center', font=("Lucida Console", "20", "bold"))
+welcome.place(anchor='n', x=362, rely=0.16)
+
+# ----- FUNCTIONS FOR EACH BUTTONS -----
+
 # frame_left.place_forget is the process of cleaning left part.
 # common_parts() reassigns the common parts.
-
-
-# TODO change the way label displayed: text variable and no unique labels.
 
 
 def common_parts():
@@ -26,85 +55,75 @@ def common_parts():
     disp_image.place(anchor="n", x=362, y=0)
 
 
+def add_student():
+    frame_left.place_forget()
+    # clear_left_frame()
+    common_parts()
+    text = 'This is add_student function'
+    label_head.config(text=text)
+    label_head.place(anchor='n', x=362, rely=0.20)
+
+    # Depreciated
+    # label_as = tkinter.Label(frame_left, text='ADD Student', font=('Lucida Console', '25', 'bold'))
+    # label_as.place(x=300, y=362)
+
+
 def add_attendance():
     frame_left.place_forget()
     common_parts()
-    text = 'Tkinter suck but gotta still try/n'
-    label.config(text=text)
+    text = "Please check-in to today's class"
+    label_head.config(text=text)
+    label_head.place(anchor='n', x=362, rely=0.20)
+    # Depreciated
     # label_aa = tkinter.Label(frame_left, text='Check-in', font=('Lucida Console', '10', 'bold'))
     # label_aa.place(x=300, y=362)
-    messagebox.showinfo(message='attendance func evoked')
 
 
 def view_record():
     frame_left.place_forget()
     common_parts()
-    label_vr = tkinter.Label(frame_left, text='View', font=('Lucida Console', '15', 'bold'))
-    label_vr.place(x=300, y=362)
-    messagebox.showinfo(message='view function evoked')
+    text = "This is view_record function"
+    label_head.config(text=text)
+    label_head.place(anchor='n', x=362, rely=0.20)
+
+    # Depreciated
+    # label_vr = tkinter.Label(frame_left, text='View', font=('Lucida Console', '15', 'bold'))
+    # label_vr.place(x=300, y=362)
 
 
 def update_record():
     frame_left.place_forget()
     common_parts()
-    label_ur = tkinter.Label(frame_left, text='Update', font=('Lucida Console', '30', 'bold'))
-    label_ur.place(x=300, y=362)
-    messagebox.showinfo(message='update_record evoked')
+    text = 'This is update_record function'
+    label_head.config(text=text)
+    label_head.place(anchor='n', x=362, rely=0.20)
+
+    # Depreciated
+    # label_ur = tkinter.Label(frame_left, text='Update', font=('Lucida Console', '30', 'bold'))
+    # label_ur.place(x=300, y=362)
+
 
 
 def delete_record():
     frame_left.place_forget()
     common_parts()
-    label_dr = tkinter.Label(frame_left, text='Delete', font=('Lucida Console', '5', 'bold'))
-    label_dr.place(x=300, y=362)
-    messagebox.showinfo(message='delete func evoked')
+    text = 'This is delete_record function'
+    label_head.config(text=text)
+    label_head.place(anchor='n', x=362, rely=0.20)
 
-# -----   Functions End
+    # Depreciated
+    # label_dr = tkinter.Label(frame_left, text='Delete', font=('Lucida Console', '5', 'bold'))
+    # label_dr.place(x=300, y=362)
 
-
-# Start of element assignment of GUI
-root = tkinter.Tk()
-
-root.title("TUJ Student Repository")
-root.geometry('1024x768')
-
-frame_left = tkinter.Frame(root, bg='#9D2235', width="724", height="768") # Temple Color
-frame_right = tkinter.Frame(root, bg='gray', width="300", height="768")
-frame_right.pack(side="right")
-frame_left.pack(side="left")
-
-text = 'This is text variable'
-label = tkinter.Label(frame_left, text=text)
-
-
-# TU Logo
-tu_image = tkinter.PhotoImage(file='temple-logo-t-box.png')
-disp_image = tkinter.Label(frame_left, image=tu_image)
-disp_image.place(anchor="n", x=362, y=0)
-# Welcome message
-welcome = tkinter.Label(frame_left, text='Welcome to TUJ Student Repository',
-                        justify='center', fg='white', bg='black',
-                        anchor='center', font=("Lucida Console", "20", "bold"))
-welcome.place(relx=0.25, rely=0.16)
-
-def add_student():
-    frame_left.place_forget()
-    # clear_left_frame()
-    common_parts()
-    text = 'This is modified text'
-    label.config(text=text)
-    label.place(relx=0.5, rely=0.5)
-
-    # label_as = tkinter.Label(frame_left, text='ADD Student', font=('Lucida Console', '25', 'bold'))
-    # label_as.place(x=300, y=362)
-    messagebox.showinfo(message='Add Student func evoked')
 
 # Making all the labels in GUI (NOT PLACING!)
 # Left Elements
 label_name = tkinter.Label(frame_left, text='Full Name ("John Doe")')
 label_id = tkinter.Label(frame_left, text='TUID')
 
-# Placing all right buttons
+
+# ----- BUTTONS -----
+
 #   Add Student Button
 button_add_student = tkinter.Button(frame_right,
                                     text='Register to Courses', fg='black', command=add_student)
@@ -134,6 +153,11 @@ button_delete.place(relx=0.13, rely=0.53, relwidth=0.8)
 button_clear = tkinter.Button(frame_right,
                               text='Exit', fg='black', command=root.quit)
 button_clear.place(relx=0.13, rely=0.6, relwidth=0.8)
+
+
+
+
+
 
 # End of element assignment of GUI
 root.mainloop()
